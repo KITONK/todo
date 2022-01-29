1e7bab2a0b3d56c0ab939a4829e7c21b6121f63b
KITONK <kiyj585@gmail.com>
2022-01-29 10:35:38 +0200

    feat(sidebar): add side bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import weather from './assets/img/cloudy.png';
+import List from './components/List';
+import calendar from './assets/img/calendar.png';
+import inbox from './assets/img/inbox.png';
+import monitor from './assets/img/monitor.png';
+import shop from './assets/img/shopping-cart.png';
+import travel from './assets/img/suitcase.png';
+import AddButtonList from './components/AddButtonList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <div className="todo__sidebar">
+        <ul className="todo__list__head">
+          <li>
+            <div className="menu">
+              <div className="circle"></div>
+              <div className="circle"></div>
+              <div className="circle"></div>
+            </div>
+            <i>
+              <img src={weather} width="20" height="20" alt="weather"/>
+            </i>
+          </li>
+        </ul>
+        <input className="todo__search" placeholder="Search..."/>
+          <List items={[
+            {
+              icon: <img src={calendar} width="20" height="20" alt="calendar"/>,
+              name: 'Today',
+              active: true
+            },
+            {
+              icon: <img src={inbox} width="20" height="20" alt="calendar"/>,
+              name: 'Inbox'
+            },
+          ]}
+            isRemovable={true}
+          />
+        <hr style={{marginTop: "15px", marginBottom: "15px"}}/>
+        <List items={[
+            {
+              icon: <img src={monitor} width="20" height="20" alt="calendar"/>,
+              name: 'Website Redesign'
+            },
+            {
+              icon: <img src={shop} width="20" height="20" alt="calendar"/>,
+              name: 'Shopping List'
+            },
+            {
+              icon: <img src={travel} width="20" height="20" alt="calendar"/>,
+              name: 'Travel'
+            },
+          ]}/>
+          <AddButtonList/>
+      </div>
+      <div className="todo_tasks">
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/AddButtonList/index.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+import './AddButtonList.css';
+
+const AddButtonList = () => {
+
+    return (
+        <div className="add-list">
+        <button className="todo__btn">Add list</button>
+        {/* <div className="add-list__popup">
+            <h1>123</h1>
+        </div> */}
+        </div>
+    );
+};
+
+export default AddButtonList;
